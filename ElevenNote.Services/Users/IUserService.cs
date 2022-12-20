94cada69979ae1ebcba7875864e89707e08d4e93
Caitlin Simon <csimondesign@gmail.com>
2022-12-20 18:21:39 -0500

    moving on to 8.0

--- ElevenNote.Services/Users/IUserService.cs
+++ ElevenNote.Services/Users/IUserService.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
+using ElevenNote.Services;
+
 namespace ElevenNote.Services.Users
 {
     public class IUserService
